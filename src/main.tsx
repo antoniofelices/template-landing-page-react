89fc18d6fdbe23b375f00dd9cb80060171393283
antoniofelices <antonio@studiomoare.com>
2025-06-05 19:44:00 +0200

    Add Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import AllRoutes from '@/routes'
+import '@/styles/main.css'
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
