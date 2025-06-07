508830f7c2f1079aa0c908b316bae3585d475f52
antoniofelices <antonio@studiomoare.com>
2025-06-07 22:40:38 +0200

    Load custom fonts

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,11 +3,12 @@ import Hero from '@/components/sections/Hero'
 import Aside from '@/components/sections/Aside'
 import heroAboutData from '@data/about/hero'
 import asideAboutData from '@data/about/aside'
+import illustrationHero from '@/assets/images/illustration-hero.svg'
 
 function About() {
     return (
         <DefaultLayout>
-            <Hero content={heroAboutData} />
+            <Hero content={heroAboutData} image={illustrationHero} />
             <Aside content={asideAboutData} />
         </DefaultLayout>
     )
